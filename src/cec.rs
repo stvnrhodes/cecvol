c249b1bac4fb94ed006e340a9f3006c74062eb03
Steven Rhodes <rhodes.steven@gmail.com>
2020-09-20 05:02:20 +0100

    messing with actors, does not compile

---
+++ src/cec.rs
@@ -0,0 +1,89 @@
+use actix::prelude::*;
+
+use cec_rs::{
+    CecCommand, CecConnection, CecConnectionCfgBuilder, CecDeviceType, CecDeviceTypeVec,
+    CecKeypress,
+};
+
+struct VolumeState {
+    current_volume: i32,
+    is_muted: bool,
+}
+
+#[derive(Message)]
+#[rtype(result = "Result<bool, std::io::Error>")]
+struct Mute {
+    mute: bool,
+}
+
+#[derive(Message)]
+#[rtype(result = "Result<bool, std::io::Error>")]
+struct SetVolume {
+    volume_level: i32,
+}
+
+#[derive(Message)]
+#[rtype(result = "Result<bool, std::io::Error>")]
+struct VolumeRelative {
+    relative_steps: i32,
+}
+
+// Messages
+//
+/// Define message
+#[derive(Message)]
+#[rtype(result = "Result<bool, std::io::Error>")]
+struct Ping;
+
+// Define actor
+pub struct CECActor {
+    conn: CecConnection,
+}
+
+// Provide Actor implementation for our actor
+impl Actor for CECActor {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Context<Self>) {
+        let cfg = CecConnectionCfgBuilder::default()
+            .port("RPI".into())
+            .device_name("Pi".into())
+            .activate_source(false) /* Don't auto-turn on the TV */
+            .device_types(CecDeviceTypeVec::new(CecDeviceType::RecordingDevice))
+            .key_press_callback(Box::new(on_key_press))
+            .command_received_callback(Box::new(on_command_received))
+            .build()
+            .unwrap();
+        let conn = cfg.open().unwrap();
+        println!("CECActor is alive");
+    }
+
+    fn stopped(&mut self, ctx: &mut Context<Self>) {
+        println!("Actor is stopped");
+    }
+}
+
+/// Define handler for `Ping` message
+impl Handler<Ping> for CECActor {
+    type Result = Result<bool, std::io::Error>;
+
+    fn handle(&mut self, msg: Ping, ctx: &mut Context<Self>) -> Self::Result {
+        println!("Ping received");
+
+        Ok(true)
+    }
+}
+
+fn on_key_press(keypress: CecKeypress) {
+    println!(
+        "onKeyPress: {:?}, keycode: {:?}, duration: {:?}",
+        keypress, keypress.keycode, keypress.duration
+    );
+}
+
+fn on_command_received(command: CecCommand) {
+    println!(
+        "onCommandReceived:  opcode: {:?}, initiator: {:?}",
+        command.opcode, command.initiator
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
-use cec_rs::CecConnectionCfgBuilder;
-fn main() {
-    let cfg = CecConnectionCfgBuilder::default();
-    println!("Hello, world!");
+use actix_web::{middleware, web, App, HttpRequest, HttpServer, Responder};
+
+mod cec;
+use cec::CECActor;
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let cecActor = CECActor::start();
+
+    HttpServer::new(|| {
+        App::new()
+            .data(cecActor)
+            .wrap(middleware::Logger::default())
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+
+    // todo
+    // read vol
+    // need to read up on google home graph
+    // allow mute
+    // for now, curl to test out each?
+    //https://developers.google.com/assistant/smarthome/develop/create#setup-server
 }
