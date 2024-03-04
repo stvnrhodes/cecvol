4da8570385c071457723fbb1d0b904ae5fddd2a4
Steven Rhodes <rhodes.steven@gmail.com>
2024-03-04 10:58:30 -0800

    remove fitbit app because I don't have a watch anymore, remove auth, change more things to be configurable, update readme, update some icons

--- src/bin/tvcli.rs
+++ src/bin/tvcli.rs
@@ -1,12 +1,31 @@
 use cecvol::lgip;
-use std::env;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Keycode for pairing the LG tv with the server.
+    #[arg(long, env = "LG_KEYCODE")]
+    keycode: String,
+
+    /// TV MAC address for WoL.
+    #[arg(long, env = "LG_MAC_ADDR")]
+    mac_addr: String,
+
+    commands: Vec<String>,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    let tv = lgip::LGTV::new("LGWebOSTV.local".to_string(), [0x64, 0x95, 0x6c, 0x06, 0x84, 0x98], "0J8FOLOW");
-    let mut cmd = args[1..].join(" ");
+    let mut mac_addr = [0u8; 6];
+    for (i, s) in args.mac_addr.split(":").enumerate() {
+        mac_addr[i] = u8::from_str_radix(s, 16)?;
+    }
+    let tv = lgip::LGTV::new("LGWebOSTV.local".to_string(), mac_addr, &args.keycode);
+    let mut cmd = args.commands.join(" ");
     cmd.push_str("\r");
+    println!("{}", cmd);
     println!("{}", tv.send_command(&cmd)?);
     Ok(())
 }
