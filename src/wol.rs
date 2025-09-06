0ee9aa916edf6deba16f232b5ad89be030ccefc7
Steven Rhodes <rhodes.steven@gmail.com>
2025-09-06 12:15:57 -0700

    Infer broadcast address

--- src/wol.rs
+++ src/wol.rs
@@ -1,6 +1,28 @@
-use std::net::UdpSocket;
+use nix::ifaddrs::getifaddrs;
+use std::io;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
 
 const SYNCHRONIZATION_SCHEME: [u8; 6] = [0xff; 6];
+
+fn get_broadcast_addr() -> io::Result<IpAddr> {
+    let addrs = getifaddrs()?;
+    for ifaddr in addrs {
+        if !ifaddr
+            .flags
+            .contains(nix::net::if_::InterfaceFlags::IFF_LOOPBACK)
+        {
+            if let Some(broadcast) = ifaddr.broadcast {
+                if let Some(broadcast_in) = broadcast.as_sockaddr_in() {
+                    return Ok(Ipv4Addr::from(broadcast_in.ip()).into());
+                }
+            }
+        }
+    }
+    Err(io::Error::new(
+        io::ErrorKind::NotFound,
+        "No suitable broadcast address found",
+    ))
+}
 
 pub fn wake(mac_address: [u8; 6]) -> std::io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0")?;
@@ -9,6 +31,8 @@ pub fn wake(mac_address: [u8; 6]) -> std::io::Result<()> {
         data.extend(&mac_address);
     }
     socket.set_broadcast(true)?;
-    socket.send_to(&data, "192.168.86.255:7")?;
+    let broadcast_addr = get_broadcast_addr()?;
+    let addr = SocketAddr::new(broadcast_addr, 7);
+    socket.send_to(&data, addr)?;
     Ok(())
 }
